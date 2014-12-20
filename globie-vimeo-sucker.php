885de2191d1ce1813522de7cbdd56c11b2d00326
cas <casska@gmail.com>
2014-12-19 19:23:57 -0600

    add - save vimeo data

--- globie-vimeo-sucker.php
+++ globie-vimeo-sucker.php
@@ -9,6 +9,9 @@
  * License: GPL2 
 */
 
+/**
+ * Adds a box to the main column on the Post (FOR NOW) edit screen.
+ */
 function globie_add_vimeo_field() {
   add_meta_box(
     'globie-video-id-meta-box',
@@ -19,6 +22,11 @@ function globie_add_vimeo_field() {
 }
 add_action( 'add_meta_boxes', 'globie_add_vimeo_field');
 
+/**
+ * Prints the Vimeo ID box.
+ *
+ * @patam WP_Post $post The object for the current post.
+ */
 function globie_vimeo_id_meta_box_callback( $post ) {
 
   // Add an nonce field so we can check for it later.
@@ -67,14 +75,49 @@ function globie_save_vimeo_id( $post_id ) {
 
   // Sanitize user input
   $vimeo_id = sanitize_text_field( $_POST['globie-vimeo-id-field'] );
+
   // Update the meta field in the database.
   update_post_meta( $post_id, '_vimeo_id_value', $vimeo_id );
-
-}
-
-add_action( 'save_post', 'globie_save_vimeo_id' );
-/*function get_vimeo_data() {
   
-}*/
+}
+add_action( 'save_post', 'globie_save_vimeo_id' );
 
+/**
+ * Gets the video data from Vimeo.
+ *
+ * @param string $video_id The ID of the video.
+ */
+function save_vimeo_data( $post_id ) {
+
+  require 'vendor/autoload.php';
+
+  $vimeo = new \Vimeo\Vimeo( 'cb3cb6706a0e37ce067cb9ec66cf17b253926a79','925468f2b2567d1ca9dd27026fcbcd4ec0e5a128' );
+  $token = $vimeo->clientCredentials();
+  $vimeo->setToken( $token['body']['access_token'] );
+
+  $video_id = get_post_meta( $post_id, '_vimeo_id_value', true );
+
+  $video_data = $vimeo->request('/videos/' . $video_id, array(), 'GET');
+
+  $video_data = $video_data['body'];
+
+  if( array_key_exists( 'error', $video_data ) ) {
+    // TODO: error handling
+  } else {
+
+    // Concat all tags
+    foreach( $video_data['tags'] as $tag ) {
+      $tags .= $tag['tag'] . ', ';
+    }
+
+    $video_post = array(
+      'ID' => $post_id,
+      'post_title' => $video_data['name'],
+      'post_content' => $video_data['description'], // TODO: check if interprets breaklines
+      'tags_input' => $tags 
+    );
+    wp_update_post( $video_post );
+  }
+}
+add_action( 'save_post', 'save_vimeo_data' );
 ?>
