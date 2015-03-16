5cb489876f8c57753c04391bc446e46d220ea3c5
Patrick Best <patrickbest@patrickbest.com>
2015-03-16 15:03:58 -0600

    syntax tiding + get largest possible thumb

--- globie-vimeo-sucker.js
+++ globie-vimeo-sucker.js
@@ -4,16 +4,14 @@ document.addEventListener("DOMContentLoaded", function() {
     e.preventDefault();
 
     vimeoId = document.getElementById('gvsucker-id-field');
-    if( vimeoId.value == '' ) {
+
+    if( vimeoId.value === '' ) {
       alert('Vimeo ID needed');
       return;
     }
 
     // Turn on spinner
     document.getElementById('globie-spinner').style.display = "inline-block";
-
-    // Get access token
-    // TODO: Get token from hidden field
 
     // Get video data
     var xmlhttp;
@@ -25,17 +23,13 @@ document.addEventListener("DOMContentLoaded", function() {
     xmlhttp.open('GET', 'https://api.vimeo.com/videos/' + vimeoId.value, true);
     xmlhttp.setRequestHeader("Authorization", "bearer a0c52130c00d1382bb992ebc59abc9cf");
     xmlhttp.onreadystatechange = function() {
-      console.log(xmlhttp.responseText);
+//       console.log(xmlhttp.responseText);
       if( xmlhttp.readyState == 4 ) {
         if( xmlhttp.status == 200 ) {
           var vimeoData = JSON.parse(xmlhttp.responseText);
 
-          console.log(vimeoData.width);
-          console.log(vimeoData.height);
-
           // Split the vimeo title
           var titleArray = vimeoData.name.split('"');
-          //         console.log(titleArray);
 
           // Tidy the split strings. input must follow exact spacing pattern for this to work
           var videoBrand = titleArray[0].substring(0, titleArray[0].length-1);
@@ -57,14 +51,20 @@ document.addEventListener("DOMContentLoaded", function() {
           window.switchEditors.switchto({id: "content-tmce"});
 
           // Set tags
-          var tagsList = '', 
+          var tagsList = '',
           whitelist = gVSucker.whitelist.toLowerCase();
           vimeoData.tags.forEach( function(tag, index, tags) {
             // Check if tag is in the whitelist
             // If the whitelist is empty all pass
-            if( !whitelist || whitelist.indexOf(tag.tag.toLowerCase() ) != -1)
+            if ( !whitelist || whitelist.indexOf(tag.tag.toLowerCase() ) != -1) {
               tagsList += tag.name + ", ";
+            }
           });
+
+          console.log(vimeoData.tags);
+          console.log(whitelist);
+          console.log(tagsList);
+
           document.getElementById('new-tag-post_tag').value = tagsList;
 
           // Set video values
@@ -76,30 +76,36 @@ document.addEventListener("DOMContentLoaded", function() {
           var inside = document.getElementById('postimagediv').getElementsByClassName('inside')[0];
           inside.innerHTML = '';
 
-          //    Set Thumbnail
+          // Set Thumbnail
           var featImg = document.createElement('img');
           featImg.setAttribute('id', 'gvsucker-img');
           featImg.setAttribute('src', vimeoData.pictures.sizes[2].link);
           featImg.setAttribute('width', 266);
           inside.appendChild(featImg);
 
-          //    Set url in hidden field
-          document.getElementById('gvsucker-img-field').value = vimeoData.pictures.sizes[4].link;
+          // Set url in hidden field
+          if (vimeoData.pictures.sizes.length === 6) {
+            vimeoThumb = vimeoData.pictures.sizes[5].link;
+          } else {
+            vimeoThumb = vimeoData.pictures.sizes[4].link;
+          }
+          document.getElementById('gvsucker-img-field').value = vimeoThumb;
         }
       } else {
         var responseText = JSON.parse(xmlhttp.responseText);
-        if( responseText.error)
+        if ( responseText.error) {
           alert("Vimeo Error: " + responseText.error);
+        }
       }
 
       // Turn off spinner
       document.getElementById('globie-spinner').style.display = "none";
-    }
+    };
     xmlhttp.send();
   });
 });
 
-//  This function selects an option in a select according to its text value
+// This function selects an option in a select according to its text value
 function selectOption(selectId, selectText) {
   var select = document.getElementById(selectId),
       selected = false;
