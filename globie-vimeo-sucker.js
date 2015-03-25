aedc083a082bbb732153bec07f17a23e85d6866c
Patrick Best <patrickbest@patrickbest.com>
2015-03-25 17:45:06 -0600

    remove console logs

--- globie-vimeo-sucker.js
+++ globie-vimeo-sucker.js
@@ -23,7 +23,6 @@ document.addEventListener("DOMContentLoaded", function() {
     xmlhttp.open('GET', 'https://api.vimeo.com/videos/' + vimeoId.value, true);
     xmlhttp.setRequestHeader("Authorization", "bearer a0c52130c00d1382bb992ebc59abc9cf");
     xmlhttp.onreadystatechange = function() {
-//       console.log(xmlhttp.responseText);
       if( xmlhttp.readyState == 4 ) {
         if( xmlhttp.status == 200 ) {
           var vimeoData = JSON.parse(xmlhttp.responseText);
@@ -60,10 +59,6 @@ document.addEventListener("DOMContentLoaded", function() {
               tagsList += tag.name + ", ";
             }
           });
-
-          console.log(vimeoData.tags);
-          console.log(whitelist);
-          console.log(tagsList);
 
           document.getElementById('new-tag-post_tag').value = tagsList;
 
