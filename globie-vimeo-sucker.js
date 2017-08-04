97059a5e3be02a1fa979c0ccb9c3c327db1fceb8
cas <cas@interglobal.vision>
2017-08-03 21:56:37 -0500

    add put video data into fields

--- globie-vimeo-sucker.js
+++ globie-vimeo-sucker.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     vimeoId = document.getElementById('gvsucker-id-field');
     if( vimeoId.value == '' ) {
-      alert('Vimeo ID needed');
+      alert(' ID needed');
       return;
     }
 
@@ -23,35 +23,33 @@ document.addEventListener("DOMContentLoaded", function() {
     } else {
       xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
     }
-    xmlhttp.open('GET', 'https://api.vimeo.com/videos/' + vimeoId.value, true);
-    xmlhttp.setRequestHeader("Authorization", "bearer a0c52130c00d1382bb992ebc59abc9cf");
+    xmlhttp.open('GET', GVS.apiUrl + 'video/' + vimeoId.value, true);
+
     xmlhttp.onreadystatechange = function() {
       if( xmlhttp.readyState == 4 ) {
         if( xmlhttp.status == 200 ) {
           var vimeoData = JSON.parse(xmlhttp.responseText);
 
-          console.log(vimeoData.width);
-          console.log(vimeoData.height);
-
           // Set title
           document.getElementById('title').focus();
-          document.getElementById('title').value = vimeoData.name;
+          document.getElementById('title').value = vimeoData.snippet.title;
 
           // Set content
           // For Visual editor
           if( document.getElementById('content-tmce') ) {
             window.switchEditors.go('content', 'html');
-            document.getElementById('content').value = vimeoData.description;
+            document.getElementById('content').value = vimeoData.snippet.description;
             window.switchEditors.go('content', 'tmce');
-            
+
           // For Text editor
           } else {
-            document.querySelector('.wp-editor-area').value = vimeoData.description;
+            document.querySelector('.wp-editor-area').value = vimeoData.snippet.description;
           }
 
+          /*
           // Set tags
-          var tagsList = '', 
-            whitelist = gVSucker.whitelist.toLowerCase();
+          var tagsList = '',
+            whitelist = GVS.whitelist.toLowerCase();
           vimeoData.tags.forEach( function(tag, index, tags) {
             // Check if tag is in the whitelist
             // If the whitelist is empty all pass
@@ -59,29 +57,29 @@ document.addEventListener("DOMContentLoaded", function() {
               tagsList += tag.name + ", ";
           });
           document.getElementById('new-tag-post_tag').value = tagsList;
+          */
 
-          // Set video values
-          document.getElementById('gvsucker-width-field').value = vimeoData.width;
-          document.getElementById('gvsucker-height-field').value = vimeoData.height;
-          document.getElementById('gvsucker-ratio-field').value = (vimeoData.height/vimeoData.width);
 
           // Set featured image
           var inside = document.getElementById('postimagediv').getElementsByClassName('inside')[0];
           inside.innerHTML = '';
 
           //    Set Thumbnail
+          var maxResThumbnail = vimeoData.snippet.thumbnails.standard;
+
           var featImg = document.createElement('img');
           featImg.setAttribute('id', 'gvsucker-img');
-          featImg.setAttribute('src', vimeoData.pictures.sizes[2].link);
+          featImg.setAttribute('src', maxResThumbnail.url);
           featImg.setAttribute('width', 266);
           inside.appendChild(featImg);
 
-          //    Set url in hidden field
-          if (vimeoData.pictures.sizes.length === 6) {
-            vimeoThumb = vimeoData.pictures.sizes[5].link;
-          } else {
-            vimeoThumb = vimeoData.pictures.sizes[4].link;
-          }
+          vimeoThumb = vimeoData.snippet.thumbnails.maxres.url;
+
+          // Set video values
+          document.getElementById('gvsucker-width-field').value = maxResThumbnail.width;
+          document.getElementById('gvsucker-height-field').value = maxResThumbnail.height;
+          document.getElementById('gvsucker-ratio-field').value = (maxResThumbnail.height/maxResThumbnail.width);
+
           // Remove any POST params in the image url
           vimeoThumb = vimeoThumb.split('?')[0];
           document.getElementById('gvsucker-img-field').value = vimeoThumb;
@@ -90,7 +88,7 @@ document.addEventListener("DOMContentLoaded", function() {
         if( xmlhttp.responseText ) {
           var responseText = JSON.parse(xmlhttp.responseText);
           if ( responseText.error) {
-            alert("Vimeo Error: " + responseText.error);
+            alert(" Error: " + responseText.error);
           }
         }
       }
